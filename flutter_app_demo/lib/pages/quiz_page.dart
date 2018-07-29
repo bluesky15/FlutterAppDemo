467213eca25f4a6ce7573fcb754f6c33fd9dc83d
lkb <lalitkbehera@gmail.com>
2018-07-29 13:52:01 +0530

    Initial flutter project added

---
+++ flutter_app_demo/lib/pages/quiz_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_demo/UI/answer_button.dart';
+import 'package:flutter_app_demo/UI/question_text.dart';
+import '../utils/question.dart';
+import '../utils/quiz.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  State createState() => new QuizPageState();
+}
+
+class QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        new Column(
+          //This is out main page
+          children: <Widget>[
+            new AnswerButton(true, () => print("you answered true")),
+            new QuestionText("Pizz is nice", 1),
+            new AnswerButton(false, () => print("you answered false"))
+          ],
+        ),
+      ],
+    );
+  }
+}
